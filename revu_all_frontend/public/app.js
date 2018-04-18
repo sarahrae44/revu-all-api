7c0a9f119c3e79daad418655b479803e26d7c59f
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2018-04-18 17:46:42 -0600

    controller working

--- revu_all_frontend/public/app.js
+++ revu_all_frontend/public/app.js
@@ -1,1 +1,7 @@
 console.log('Front-end working');
+
+var app = angular.module('reviewboard', []);
+
+app.controller('mainController', ['$http', function($http) {
+  this.message = "controller works"
+}]);
