f3ee78460c06f1e3973da41f8556e59c93bb8bfb
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2018-04-18 17:40:53 -0600

    front end up and running

--- revu_all_frontend/server.js
+++ revu_all_frontend/server.js
@@ -1,2 +1,9 @@
 var express = require('express');
 var app     = express();
+var port    = 3001;
+
+app.use(express.static('public'));
+
+app.listen(3001, function() {
+  console.log("Noticeboard Frontend running on port: ", port);
+});
