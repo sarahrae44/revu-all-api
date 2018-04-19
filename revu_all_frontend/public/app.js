cb37d9f164fab9fb5c4939f48c943bb0aa61465e
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2018-04-19 08:21:45 -0600

    able to submit review

--- revu_all_frontend/public/app.js
+++ revu_all_frontend/public/app.js
@@ -3,13 +3,22 @@ console.log('Front-end working');
 var app = angular.module('reviewboard', []);
 
 app.controller('mainController', ['$http', function($http) {
-  this.message = "controller works"
+  this.message = "controller works";
+  this.reviews = [];
+  this.formdata = {};
 
   $http({
   method: 'GET',
   url: 'http://localhost:3000/reviews',
     }).then(function(response) {
       console.log(response);
-    });
+      this.reviews = response.data;
+      console.log(this.reviews);
+    }.bind(this));
+
+    this.processForm = function() {
+    	console.log('processForm function . . .');
+    	console.log('Formdata: ', this.formdata);
+    }
 
 }]);
