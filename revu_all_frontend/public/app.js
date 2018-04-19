ded88b17b12e17bd393013c2d1795f408303b784
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2018-04-18 18:16:20 -0600

    front end and API communicating

--- revu_all_frontend/public/app.js
+++ revu_all_frontend/public/app.js
@@ -4,4 +4,12 @@ var app = angular.module('reviewboard', []);
 
 app.controller('mainController', ['$http', function($http) {
   this.message = "controller works"
+
+  $http({
+  method: 'GET',
+  url: 'http://localhost:3000/reviews',
+    }).then(function(response) {
+      console.log(response);
+    });
+
 }]);
